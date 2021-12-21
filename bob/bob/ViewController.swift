47bbe917df74748972ebbc9672aaae3146b085f2
Marcin Polak <marcin.polak@dazn.com>
2021-12-21 09:45:43 +0100

    Init empty project

---
+++ bob/bob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bob
+//
+//  Created by Marcin Polak on 21/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
